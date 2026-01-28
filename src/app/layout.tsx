c7d8f5f5c6ba493a1c594a2b1ef9840d07303007
lomesh takoory <lomesh.takoory1@umail.uom.ac.mu>
2026-01-28 20:25:30 +0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Shield } from "lucide-react"; // 1. Import the icon
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
